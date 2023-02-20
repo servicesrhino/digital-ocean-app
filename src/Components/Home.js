71d7db788d922c1ab875eb6ef8e17f2df93b562d
servicesrhino <it.services.rhino@gmail.com>
2023-02-20 23:21:36 +0200

    login translated to russian language

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -5,19 +5,19 @@ const Home = () => {
   return (
     <div>
      <Container className='small-container'>
-      <h1 className='my-3'>Sign In</h1>
+      <h1 className='my-3'>Логин</h1>
       <Form>
         <FormGroup className='mb-3' controlId='email' >
-          <Form.Label>Email</Form.Label>
+          <Form.Label>Номер телефона</Form.Label>
           <Form.Control type='email' required />
         </FormGroup>
 
         <FormGroup className='mb-3' controlId='password' >
-          <Form.Label>Password</Form.Label>
+          <Form.Label>Пароль</Form.Label>
           <Form.Control type='password' required />
         </FormGroup>
         <div className='mb-3'>
-          <Button type='submit' >Sign In</Button>
+          <Button type='submit' >Логин</Button>
         </div>
       </Form>
       </Container>

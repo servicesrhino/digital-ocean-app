84d8c3c4fd436b45c7b2939e35265c166039d088
servicesrhino <it.services.rhino@gmail.com>
2023-02-21 11:33:09 +0200

    added vizualization of table data  on site

--- src/Components/ParseExcel.js
+++ src/Components/ParseExcel.js
@@ -1,12 +1,8 @@
 import React, { useState } from 'react';
-
-//import { read, readFile, utils, writeFile, XLSX } from 'xlsx';
-//import XLSX from 'xlsx';
-//import { read, writeFileXLSX } from "./xlsx.mjs";
-//import * as XLSX from 'xlsx/xlsx.mjs';
-
-
+import { Row, Table, Col, Label } from 'react-bootstrap';
 import { read, utils, writeFileXLSX } from "xlsx";
+
+
 
 // /* load the codepage support library for extended support with older formats  */
 // import { set_cptable } from "xlsx";
@@ -16,17 +12,41 @@ import { read, utils, writeFileXLSX } from "xlsx";
 const ParseExcel = () => {
 
     const [fileName, setFileName] = useState(null);
+    const [allFile, setAllFile] = useState(null);
+    const [other, setOther] = useState(null);
+
+    const [sheetData, setSheetData] = useState([]);
+    const [sheet, setSheet] = useState(null);
+
+    const [columns, setColumns] = useState([]);
+    const [body, setBody] = useState([]);
 
     const handleFile = async (e) => {
         const file = e.target.files[0];
+        if (!file ) return;
 
         setFileName(file.name);
+        setAllFile(file);
+        console.log(file);
 
         const data = await file.arrayBuffer();
+        console.log(data);
         const workbook = read(data);
+        setOther(workbook.Sheet);
+        console.log(workbook.Sheet);
 
         const worksheet = workbook.Sheets[workbook.SheetNames[0]]
-        const jsonData = utils.sheet_to_json(worksheet)
+        console.log(worksheet);
+        const jsonData = utils.sheet_to_json(worksheet, {
+            blankrows: "",
+            header: 1
+        });
+
+        setSheet(Object.keys(e)[0]);
+        setSheetData(jsonData);
+
+        setColumns(jsonData[0])
+        setBody(jsonData.shift());
 
         // e.stopPropagation(); e.preventDefault();
         // const f = e.dataTransfer.files[0];
@@ -40,12 +60,42 @@ const ParseExcel = () => {
   return (
     <div>
       <h1>Parse Excel</h1>
+      {!fileName && <div>Пожалуйста, загрузите файл</div>}
       {fileName && (
         <p>
             File name: <span>{fileName}</span>
         </p>
       )}
       <input type="file" onChange={(e) => handleFile(e)} />
+      <Row>
+        <Col md={12}>
+            <Table bordered className='border'>
+                <thead className='text-primary table-header'>
+                    <tr>
+                    {columns.map(c => (
+                    // <div key={c}>{c}</div>
+                    <td key={c}>{c}</td>
+                         ))}
+                    </tr>
+
+                </thead>
+
+                <tbody className='table-body'>
+              {/* <tr> */}
+              {sheetData.slice(1).map((row) => (
+            // <div key={c}>{c}</div>
+            <tr >
+              {row.map(c => <td>{c} </td> )}
+            </tr>
+          ))}
+
+              {/* </tr> */}
+            </tbody>
+
+            </Table>
+
+        </Col>
+      </Row>
     </div>
   )
 }

52e8a0385ddc41366756c76b68322cb4dacb8fc2
servicesrhino <it.services.rhino@gmail.com>
2025-01-29 14:10:00 -0500

    some changes

--- src/Inventorization/Inventorization.js
+++ src/Inventorization/Inventorization.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import Sidebar from '../Components/Sidebar/Sidebar';
 import axios from 'axios';
 import { Col, Row, Table } from 'react-bootstrap';
@@ -6,6 +6,8 @@ import './Inventorization.css';
 
 const Inventorization = () => {
   const [data, setData] = useState([]);
+  const [container, setContainer] = useState([]);
+
   const getData = () => {
     const result = axios
       .get('https://rhino-api-dyq7j.ondigitalocean.app/Inventory/gelList')
@@ -15,55 +17,111 @@ const Inventorization = () => {
       });
   };
   console.log('some:', data);
+
+  const data2 = () => {
+    const result2 = axios
+      .post(
+        'https://rhino-api-dyq7j.ondigitalocean.app/GoogleSheet/get-last-documents-list'
+      )
+      .then((res) => {
+        console.log(res.data);
+        const newData = res.data.map((doc) => {
+          return {
+            ...doc,
+            created: doc.created.slice(0, doc.created.indexOf('T')),
+          };
+        });
+        setContainer(newData);
+      });
+  };
+
+  useEffect(() => {
+    if (!data) return;
+    data2();
+  }, [data]);
+
+  console.log('container:', container);
   return (
     <div className="appss">
       <div className="appss__body">
         <Sidebar />
         <div className="app__other">
-          <button className="btn btn-primary" onClick={getData}>
-            Переобліки
-          </button>
+          <div>
+            <button className="btn btn-primary" onClick={getData}>
+              Переобліки
+            </button>
 
-          {data.status === 'open' ? <button className="m-2">open</button> : ''}
-          {/* <button>Создать переучет</button> */}
+            {data.status === 'open' ? (
+              <button className="m-2">open</button>
+            ) : (
+              ''
+            )}
+            <button>some</button>
+            {/* <button>Создать переучет</button> */}
 
-          <Row>
-            <Col>
-              <Table hover>
-                <thead className="text-right table-header text-header">
-                  <tr>
-                    <th>Дата</th>
-                    {/* <tfewxxh>ID</tfewh> */}
-                    <th>Nомер</th>
-                    <th>Статус</th>
-                    <th></th>
-                  </tr>
-                </thead>
-
-                <tbody className="text-secondary table-body">
-                  {data.map((getData, index) => (
-                    <tr key={index}>
-                      <th>{getData.date.slice(0, 10)}</th>
-                      {/* <th>{getData.id}</th> */}
-                      <th>{getData.number}</th>
-                      <th>{getData.status}</th>
-                      <th>
-                        {data.status === 'open' ? (
-                          <button disabled>закрыть переучет</button>
-                        ) : (
-                          <button className="btn btn-danger">
-                            закрыть переучет
-                          </button>
-                        )}
-                        {/* if (data.status === 'open')
-                        {<button>Закрыть переучет</button>} */}
-                      </th>
+            <Row>
+              <Col>
+                <Table hover>
+                  <thead className="text-right table-header text-header">
+                    <tr>
+                      <th>Дата</th>
+                      {/* <tfewxxh>ID</tfewh> */}
+                      <th>Nомер</th>
+                      <th>Статус</th>
+                      <th></th>
                     </tr>
-                  ))}
-                </tbody>
-              </Table>
-            </Col>
-          </Row>
+                  </thead>
+
+                  <tbody className="text-secondary table-body">
+                    {data.map((getData, index) => (
+                      <tr key={index}>
+                        <th>{getData.date.slice(0, 10)}</th>
+                        {/* <th>{getData.id}</th> */}
+                        <th>{getData.number}</th>
+                        <th>{getData.status}</th>
+                        <th>
+                          {data.status === 'open' ? (
+                            <button disabled>закрыть переучет</button>
+                          ) : (
+                            <button className="btn btn-danger">
+                              закрыть переучет
+                            </button>
+                          )}
+                          {/* if (data.status === 'open')
+                        {<button>Закрыть переучет</button>} */}
+                        </th>
+                      </tr>
+                    ))}
+                  </tbody>
+                </Table>
+              </Col>
+            </Row>
+          </div>
+          {/* <button>Відкрити контейнери</button> */}
+          <div>
+            <Row>
+              <Col>
+                <Table>
+                  <thead>
+                    <tr>
+                      <th>Дата</th>
+                      <th>Назва документу</th>
+                    </tr>
+                  </thead>
+
+                  <tbody>
+                    {container &&
+                      container.map((getData, index) => (
+                        <tr key={index}>
+                          <td>{getData.created}</td>
+                          <td>{getData.documentName}</td>
+                        </tr>
+                      ))}
+                  </tbody>
+                </Table>
+              </Col>
+            </Row>
+          </div>
         </div>
       </div>
     </div>

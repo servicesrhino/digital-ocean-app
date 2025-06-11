b5d93295e6f3b4d9d3aa930253eaaf87ca1b9e10
servicesrhino <it.services.rhino@gmail.com>
2025-06-11 15:08:31 -0400

    allParts2

--- src/Components/AllParts.js
+++ src/Components/AllParts.js
@@ -3,17 +3,110 @@ import { Button, Col, Row, Table } from 'react-bootstrap';
 import $api from './http';
 import Sidebar from './Sidebar/Sidebar';
 import axios from 'axios';
+// import { DataGridPro } from '@mui/x-data-grid';
+import { DataGridPro, GridToolbar } from '@mui/x-data-grid-pro';
+import { Button as Button2, Box } from '@mui/material';
 // import { Button } from '@mui/material';
+
+import { GridToolbarQuickFilter } from '@mui/x-data-grid-pro';
+// import { Box } from '@mui/material';
 
 function AllParts() {
   const [allData, setAllData] = useState([]);
-  const getParts = () => {
+  const [quickFilter, setQuickFilter] = useState('');
+
+  const CustomToolbar = () => {
+    return (
+      <Box
+        sx={{
+          display: 'flex',
+          justifyContent: 'flex-start', // 👈 aligns to the left
+          p: 1,
+        }}
+      >
+        <GridToolbarQuickFilter debounceMs={500} />
+      </Box>
+    );
+  };
+
+  const columns = [
+    { field: 'id', headerName: 'ID', width: 90 },
+    {
+      field: 'name',
+      headerName: 'name',
+      width: 150,
+      editable: true,
+    },
+    {
+      field: 'rhinoID',
+      headerName: 'rhinoID',
+      width: 150,
+      editable: true,
+    },
+    {
+      field: 'originalIDs',
+      headerName: 'originalIDs',
+      type: 'number',
+      width: 110,
+      editable: true,
+    },
+    {
+      field: 'qrCode',
+      headerName: 'qrCode',
+      type: 'number',
+      width: 110,
+      editable: true,
+    },
+    // {
+    //   field: 'defect',
+    //   headerName: 'defect',
+    //   type: 'number',
+    //   width: 110,
+    //   editable: true,
+    // },
+    {
+      field: 'status',
+      headerName: 'status',
+      type: 'number',
+      width: 110,
+      editable: true,
+    },
+    {
+      field: 'location',
+      headerName: 'location',
+      type: 'number',
+      width: 110,
+      editable: true,
+    },
+    // {
+    //   field: 'fullName',
+    //   headerName: 'Full name',
+    //   description: 'This column has a value getter and is not sortable.',
+    //   sortable: false,
+    //   width: 160,
+    //   valueGetter: (value, row) =>
+    //     `${row.firstName || ''} ${row.lastName || ''}`,
+    // },
+  ];
+
+  // const rows = [
+  //   { id: 1, lastName: 'Snow', firstName: 'Jon', age: 14 },
+  //   { id: 2, lastName: 'Lannister', firstName: 'Cersei', age: 31 },
+  //   { id: 3, lastName: 'Lannister', firstName: 'Jaime', age: 31 },
+  //   { id: 4, lastName: 'Stark', firstName: 'Arya', age: 11 },
+  //   { id: 5, lastName: 'Targaryen', firstName: 'Daenerys', age: null },
+  //   { id: 6, lastName: 'Melisandre', firstName: null, age: 150 },
+  //   { id: 7, lastName: 'Clifford', firstName: 'Ferrara', age: 44 },
+  //   { id: 8, lastName: 'Frances', firstName: 'Rossini', age: 36 },
+  //   { id: 9, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
+  // ];
+  const getParts = (searchTerm) => {
     try {
       const res = axios
         .post('https://rhino-api-dyq7j.ondigitalocean.app/Parts/all', {
           page: 0, // '1IWS5aNEnsJdPG7y2GxMZJkxSNP0wov1bhezsi6hWWx0',
           pageSize: 0,
-          searchTerm: 'фара',
+          searchTerm: searchTerm,
         })
         .then((res) => {
           const response = res.data;
@@ -26,27 +119,35 @@ function AllParts() {
     }
   };
 
-  useEffect(() => {
-    const getParts = () => {
-      try {
-        const res = axios
-          .post('https://rhino-api-alquo.ondigitalocean.app/Parts/all', {
-            page: 0, // '1IWS5aNEnsJdPG7y2GxMZJkxSNP0wov1bhezsi6hWWx0',
-            pageSize: 0,
-            searchTerm: 'фара',
-          })
-          .then((res) => {
-            const response = res.data;
-            console.log(res.data);
-            setAllData(response);
-          });
-      } catch (error) {
-        console.log(error);
-      }
-    };
+  // useEffect(() => {
+  //   const getParts = () => {
+  //     try {
+  //       const res = axios
+  //         .post('https://rhino-api-alquo.ondigitalocean.app/Parts/all', {
+  //           page: 0, // '1IWS5aNEnsJdPG7y2GxMZJkxSNP0wov1bhezsi6hWWx0',
+  //           pageSize: 0,
+  //           searchTerm: 'фара',
+  //         })
+  //         .then((res) => {
+  //           const response = res.data;
+  //           console.log(res.data);
+  //           setAllData(response);
+  //         });
+  //     } catch (error) {
+  //       console.log(error);
+  //     }
+  //   };
 
-    getParts();
-  }, []);
+  //   getParts();
+  // }, []);
+
+  // useEffect(() => {
+  //   const delayDebounce = setTimeout(() => {
+  //     getParts(quickFilter);
+  //   }, 500); // debounce to match quickFilterProps
+
+  //   return () => clearTimeout(delayDebounce);
+  // }, [quickFilter]);
 
   return (
     <div className="app">
@@ -54,56 +155,75 @@ function AllParts() {
         <Sidebar />
         <div className="app__other">
           <div className="my-3 mx-2">
-            <Button onClick={getParts}>Get parts</Button>
+            {/* <Button onClick={getParts}>Get parts</Button> */}
           </div>
 
-          <Row>
-            <Col md={12}>
-              <Table bordered className="border">
-                <thead className="text-primary table-header">
-                  <tr>
-                    {/* <th>
-                  <input
-                    type="checkbox"
-                    name="allselect"
-                    placeholder="some"
-                    //checked={!sheetData2.some((row) => row.defect !== true)}
-                    //onChange={handleChange}
-                  />
-                </th> */}
+          <Button2
+            variant="contained"
+            onClick={() => getParts(quickFilter)}
+            sx={{ mb: 2 }}
+          >
+            Get parts
+          </Button2>
 
-                    <th>Авто</th>
-                    <th>Наименование</th>
-                    <th>Количество</th>
-                    <th>НомерRhino</th>
-                    <th>оригинальный номер</th>
-                    <th>цена со склада</th>
-                    <th>цена входящая</th>
-                    <th>цена с амортизацией</th>
-                    <th>дата завоза</th>
+          <DataGridPro
+            rows={allData}
+            columns={columns}
+            slots={{ toolbar: CustomToolbar }}
+            slotProps={{
+              toolbar: {
+                showQuickFilter: true,
+                quickFilterProps: { debounceMs: 0 },
+              },
+            }}
+            // components={{ Toolbar: GridToolbar }}
+            // componentsProps={{
+            //   toolbar: {
+            //     showQuickFilter: true,
+            //     quickFilterProps: { debounceMs: 500 },
+            //   },
+            // }}
+            // showToolbar
+            // slots={{ toolbar: GridToolbar }}
 
-                    {/* {sheet.map(h => <td>{h}</td>)} */}
-                  </tr>
-                </thead>
-                <tbody className="text-secondary table-body">
-                  {allData.map((getdata, index) => (
-                    <tr key={index}>
-                      {/* <th>{getdata.id}</th> */}
-                      <th>{getdata.vehicleItem.name}</th>
-                      <th>{getdata.name}</th>
-                      <th>{getdata.quantity}</th>
-                      <th>{getdata.rhinoID}</th>
-                      <th>{getdata.originalID}</th>
-                      <th>{getdata.incomePrice}</th>
-                      <th>{getdata.stockPrice}</th>
-                      <th>{getdata.priceWithDepreciation}</th>
-                      <th>{getdata.deliveryInfo}</th>
-                    </tr>
-                  ))}
-                </tbody>
-              </Table>
-            </Col>
-          </Row>
+            // slotProps={{
+            //   toolbar: {
+            //     showQuickFilter: true,
+            //     quickFilterProps: { debounceMs: 500 },
+            //   },
+            // }}
+            initialState={{
+              pagination: {
+                paginationModel: {
+                  pageSize: 20,
+                },
+              },
+            }}
+            // onFilterModelChange={(filterModel) => {
+            //   console.log(
+            //     'Quick filter values:',
+            //     filterModel.quickFilterValues
+            //   );
+            // }}
+            // onFilterModelChange={(model) => {
+            //   const input = model.quickFilterValues?.[0] || '';
+            //   setQuickFilter(input);
+            // }}
+            // onFilterModelChange={(model) => {
+            //   const input = model.quickFilterValues?.[0] || '';
+            //   setQuickFilter(input);
+            // }}
+            onFilterModelChange={(model) => {
+              const input = model.quickFilterValues?.join(' ') || '';
+              setQuickFilter(input);
+            }}
+            checkboxSelection
+            disableRowSelectionOnClick
+            pageSizeOptions={[5]}
+            // checkboxSelection
+            // disableRowSelectionOnClick
+            disableColumnResize
+          />
         </div>
       </div>
     </div>
